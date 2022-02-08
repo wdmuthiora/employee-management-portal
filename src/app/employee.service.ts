8a327b58413b20b9c77e7685a384ed16a6ba0826
wdmuthiora <wdmuthiora@gmail.com>
2022-02-08 14:46:32 +0300

    Angular app with service to fetch from local cross domain SpringBoot API

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,15 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Employee } from './employee';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({
+  providedIn: 'root'
+})
+  
 export class EmployeeService {
-  private apiServerUrl = '';
-  constructor(private http: HttpClient) {}
+  
+  private apiServerUrl = environment.apiBaseUrl;
+  
+  constructor(private http: HttpClient) { }
 
   public getEmployees(): Observable<Employee[]> {
-    return this.http.get<any>(`${this.apiServerUrl}/employee/all`);
+    return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
   }
 
   public addEmployee(employee: Employee): Observable<Employee> {
@@ -17,14 +23,12 @@ export class EmployeeService {
   }
 
   public updateEmployee(employee: Employee): Observable<Employee> {
-    return this.http.put<any>(`${this.apiServerUrl}/employee/update`, employee);
+    return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`, employee);
   }
 
   public deleteEmployee(employeeId: number): Observable<void> {
     return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
   }
 
-  // public getEmployees(): Observable<Employee[]> {
-  //   return this.http.get<any>(`${this.apiServerUrl}/ employee / all`);
-  // }
+
 }
